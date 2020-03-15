5afff4e1c18db11cc943a74d6a7b6152be25ea4b
Noah Lone <noah.lonen@gmail.com>
2020-03-15 19:34:36 -0400

    Added phase_drifter which is the folder for main project. Also commited a change in test_level.

---
+++ phase_drifter/scripts/Scene2.js
@@ -0,0 +1,87 @@
+var play;
+var score;
+var about;
+var pointer;
+
+class Scene2 extends Phaser.Scene {
+    constructor() {
+        super("playGame");
+    }
+
+    preload ()
+    {
+        this.load.image('sky', '../assets/title_background.png');
+        this.load.image('play', '../assets/start.png');
+        this.load.image('logo', '../assets/final_logo.jpg');
+        this.load.image('score', '../assets/highscore.png');
+        this.load.image('about', '../assets/about.png');
+        this.load.image('pointer', '../assets/pointer.png');
+    }
+
+    create ()
+    {
+        this.add.image(400, 300, 'sky');
+        this.add.image(400, 100, 'logo');
+        play = this.add.image(400, 300, 'play');
+        score = this.add.image(400, 400, 'score');
+        about = this.add.image(400, 500, 'about');
+        pointer = this.add.image(100, 100, 'pointer');
+        pointer.setVisible(false);
+
+        // set images as interactive
+        play.setInteractive();
+        score.setInteractive();
+        about.setInteractive();
+
+        // Start button activity
+        play.on("pointerover", ()=>{
+            pointer.setVisible(true);
+            pointer.x=290;
+            pointer.y=300;
+        })
+
+        play.on("pointerout", ()=>{
+            pointer.setVisible(false);
+        })
+
+        play.on("pointerup", ()=>{
+            console.log("play clicked");
+        })
+
+        // Highscpre button activity
+        score.on("pointerover", ()=>{
+            pointer.setVisible(true);
+            pointer.x=250;
+            pointer.y=400;
+        })
+
+        score.on("pointerout", ()=>{
+            pointer.setVisible(false);
+        })
+
+        score.on("pointerup", ()=>{
+            console.log("score clicked");
+        })
+
+        // About button activity
+        about.on("pointerover", ()=>{
+            pointer.setVisible(true);
+            pointer.x=290;
+            pointer.y=500;
+        })
+
+        about.on("pointerout", ()=>{
+            pointer.setVisible(false);
+            pointer.x=290;
+            pointer.y=500;
+        })
+
+        about.on("pointerup", ()=>{
+            console.log("about clicked");
+        })
+    }
+
+    update ()
+    {
+    }
+}
---
+++ phase_drifter/scripts/testLevel.js
@@ -0,0 +1,24 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    pixelArt: true,
+    parent: "gameWindow",
+    scene: [Scene1, Scene2]
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+}
+
+function create ()
+{
+
+}
+
+function update ()
+{
+
+}

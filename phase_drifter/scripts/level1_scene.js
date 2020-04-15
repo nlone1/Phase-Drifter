73d1423637766515c2fdc5103ea989c0d370e05d
Noah Lone <noah.lonen@gmail.com>
2020-04-14 23:10:17 -0400

    Added key and spikes to level. Also added a partially complete death mechanic on spike collision

--- phase_drifter/scripts/level1_scene.js
+++ phase_drifter/scripts/level1_scene.js
@@ -11,6 +11,7 @@ class level1_scene extends Phaser.Scene {
         this.load.image('plat4', '../assets/level_1/tile4.png'); // center pixel = 12
         this.load.image('plat5', '../assets/level_1/tile5.png'); // center pixel = 80
         this.load.image('key', '../assets/level_1/key.png');
+        this.load.image('spikes', '../assets/level_1/spikes.png'); // center pixe; = 47
         this.load.spritesheet('dude', '../assets/dude.png', {
             frameWidth: 32,
             frameHeight: 36
@@ -24,7 +25,7 @@ class level1_scene extends Phaser.Scene {
         // Create a static physics group and assign it to platforms
         platforms = this.physics.add.staticGroup();
 
-        // create platforms
+        // Add platforms to level
         platforms.create(155, 150, 'plat1');
         platforms.create(367,220, 'plat2');
         platforms.create(477,150, 'plat2');
@@ -38,12 +39,22 @@ class level1_scene extends Phaser.Scene {
         platforms.create(237,505, 'plat4');
         platforms.create(202,540, 'plat4');
         platforms.create(80,540, 'plat5');
+        
+        // Create spikes as static grouop and add them to level
+        spikes = this.physics.add.staticGroup();
+        spikes.create(367, 183, 'spikes');
+        spikes.create(587, 183, 'spikes');
+        
+        // Create key amd add to level
+        key = this.physics.add.staticGroup();
+        key.create(475, 275, 'key');
 
         // Create player
         player = this.physics.add.sprite(176, 80, 'dude');
         player.setCollideWorldBounds(true);
         player.setSize(22,36);
 
+        // Animations for dude spritesheet
         this.anims.create({
             key: 'left',
             frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
@@ -81,15 +92,31 @@ class level1_scene extends Phaser.Scene {
             frameRate: 5,
             repeat: -1
         });
+        
+        // set hasKey to false at beginning of level and set the keyText
+        hasKey = false;
+        keyText = this.add.text(16, 16, 'Key: false', { fontSize: '32px', fill: '#000' });
 
         // create cursor which is Phaser's built in keyboard manager (suppliments event listeners)
         cursors = this.input.keyboard.createCursorKeys();
 
-        this.physics.add.collider(player, platforms);
+        // check for collision against key and player. If collision detected, call collectKey() from main_scene.js
+        this.physics.add.overlap(player, key, collectKey, null, this);
 
+        // Check for collision against platforms and player
+        this.physics.add.collider(player, platforms);
+        
+        // check for collision against player and spikes, if found call hitSpike() from main_scene
+        this.physics.add.collider(player, spikes, hitSpike, null, this);
     }
 
     update() {
+        
+        if(gameOver) {
+            return;
+        }
+
+        // Controller code for player. These conditionals are checking what key is pressed to execute which animation.
         if (cursors.left.isDown)
         {
             player.setVelocityX(-160);
--- phase_drifter/scripts/main_scene.js
+++ phase_drifter/scripts/main_scene.js
@@ -16,8 +16,13 @@ var config = {
 
 var game = new Phaser.Game(config);
 var platforms;
+var spikes;
 var player;
 var cursors;
+var key;
+var hasKey;
+var keyText;
+var gameOver = false;
 
 function preload ()
 {
@@ -29,5 +34,20 @@ function create ()
 
 function update ()
 {
+}
 
+
+// Function used to disable key's physics body and make it invisible. 
+function collectKey(player, key) {
+    key.disableBody(true, true);
+    
+    hasKey = true;
+    keyText.setText('Key: true');
+}
+
+function hitSpike(player, spike) {
+    this.physics.pause();
+    player.setTint(0xff0000);
+    player.anims.play('jump');
+    gameOver = true;
 }
